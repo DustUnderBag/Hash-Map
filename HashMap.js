58965b9f46ad72171884cd193f1d9b93a65a5bc0
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2025-08-31 18:30:11 -0400

    Implement `set()`: Add key-value pair into map
    `set(key, value)`: Add the given key-value pair into the hashmap.
    It uses the hashCode to store that pair into a dedicated bucket.
    If bucket is empty, insert to the head of the bucket.
    If a head exists in that bucket, add the pair to the end of it.
    
    Feature yet to be added: Triggers hashmap resizing by load factor.

--- HashMap.js
+++ HashMap.js
@@ -1,7 +1,15 @@
+import { LinkedList } from "./Linked-List/LinkedList.js";
+
 class HashMap {
     capacity = 16;
     loadFactor = 0.75;
-    bucket = new Array(this.capacity);
+    buckets = new Array(this.capacity);
+
+    constructor() {
+        for(let i = 0; i < this.capacity; i++) {
+            this.buckets[i] = new LinkedList();
+        }
+    }
 
     #hash(key) {
         let hashCode = 0;
@@ -18,8 +26,28 @@ class HashMap {
 
     set(key, value) {
         const hash = this.#hash(key);
-        console.log("Hash: ", {hash, key, value});
-        this.bucket[hash] = {key, value};
+        const bucket = this.buckets[hash];
+        const pair = {hash, key,  value};
+        console.log(pair);
+        
+        let tmp = bucket.head;
+        //If bucket is empty.
+        if(tmp == null) {
+            bucket.prepend(pair);
+            return;
+        }
+
+        //If bucket is NOT empty...
+        //If it has repeated key in any node, replace its value with new value.
+        //Else, append this node at the list's tail.
+        while(tmp.nextNode != null && tmp.value.key != key) {
+            tmp = tmp.nextNode;
+        }
+        if(tmp.value.key == key) {
+            tmp.value = pair;
+        } else {
+            bucket.append(pair);
+        }
     }
 }
 
@@ -38,4 +66,8 @@ test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
 
-console.log(test);
+test.set('elephant', 'PINK!!!!!');
+
+for(let i = 0; i < test.buckets.length; i++) {
+    console.log(test.buckets[i])
+}
---
+++ Linked-List/LinkedList.js
@@ -0,0 +1,180 @@
+import { Node } from "./Node.js";
+
+export class LinkedList {
+    #head = null;
+
+    get head() {
+        return this.#head;
+    }
+
+    get tail() {
+        //Empty list
+        if(this.#head == null) return null;
+        
+        let tmp = this.#head;
+
+        while(tmp.nextNode != null) {
+            tmp = tmp.nextNode;
+        }
+        return tmp;
+    }
+
+    get size() {
+        if(this.#head == null) return 0;
+
+        let count = 1;
+        let tmp = this.#head;
+        while(tmp.nextNode != null) {
+            tmp = tmp.nextNode;
+            count++;
+        }
+        return count;
+    }
+
+    append(value) {
+       if(this.#head == null) {
+        this.prepend(value);
+        return;
+       }
+       
+       this.tail.nextNode = new Node(value, null);
+    }
+    
+    prepend(value) {
+        this.#head = new Node(value, this.#head);
+    }
+
+    pop() {
+        if(this.size === 0) return;
+
+        //If list has only ONE element
+        if(this.#head.nextNode == null) {
+            this.#head = null;
+            return;
+        }
+        
+        let tmp = this.#head;
+        while(tmp.nextNode.nextNode != null) {
+            tmp = tmp.nextNode;
+        }
+        tmp.nextNode = null;
+    }
+
+    contains(value) {
+        if(this.size === 0) return false;
+
+        let tmp = this.#head;
+        /*
+        Traverser cannot go past last node, and stops if it 
+        finds the matching value.
+        */
+        while( tmp.nextNode != null && tmp.value !== value) {
+            tmp = tmp.nextNode;
+        }
+
+        console.log("Checking: ", tmp.value);
+        return tmp.value === value;
+        /*
+        Case 1: Reaches last node, can't find target value
+          - returns false;
+        Case 2: Reaches last node, finds target value.
+          - returns true;
+        Case 3: Hasn't reached last node, finds target value.
+          - returns true;
+        */
+    }
+
+    find(value) {
+        if(list.size === 0) return null;
+
+        let tmp = this.#head;
+        let index = 0;
+
+        while(tmp.nextNode !== null ) {
+            if(tmp.value === value) return index;
+
+            tmp = tmp.nextNode;
+            index++;
+        }
+
+        return (tmp.value === value) ? index : null;
+    }
+
+    at(index) {
+        if(this.#head == null) return null;
+        if(index > this.size - 1 || index < 0) return null;
+
+        let tmp = this.#head;
+        let step = 0;
+
+        while(tmp.nextNode != null && step < index) {
+            tmp = tmp.nextNode;
+            step++;
+        }
+        return tmp;
+    }
+
+    insertAt(value, index) {
+        if(index <= 0) {
+            this.prepend(value);
+            return;
+        }
+
+        if(index > list.size - 1) {
+            list.append(value);
+            return;
+        }
+
+        let previous;
+        let current = this.#head;
+        let step = 0;
+        while(current.nextNode != null && step < index) { 
+            previous = current;
+            current = current.nextNode;
+            step++;
+        }
+        previous.nextNode =  new Node(value, current);
+    }
+
+    removeAt(index) {
+        if(this.#head == null) return;
+
+        if(index <= 0) { 
+            this.#head = this.#head.nextNode;
+            return;
+        }
+
+        if(index >= this.size - 1) {
+            this.pop();
+            return;
+        }
+
+        if(index <= 0) { 
+            this.#head = this.#head.nextNode;
+            return;
+        }
+
+        let previous;
+        let current = this.#head;
+        let step = 0;
+        while(step < index) {
+            previous = current;
+            current = current.nextNode;
+            step++;
+        }
+        previous.nextNode = current.nextNode;
+    }
+
+    toString() {
+        if(this.#head == null) return "null";
+
+        let tmp = this.#head;
+        let str = `( ${tmp.value} )`
+        while(tmp.nextNode !== null) {
+            tmp = tmp.nextNode;
+            str += ` -> ( ${tmp.value} )`;
+        }
+        str += ` -> null`;
+        return str;
+    }
+}
